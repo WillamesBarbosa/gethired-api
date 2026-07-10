882af849350b91f075e1ad41092281b1923c12fc
WillamesBarbosa <willamessilva1999@gmail.com>
2026-07-09 21:15:42 -0300

    feat(UserService): add update and delete methods to UsersService
    - Add update method with partial fields and password hashing
    - Add delete method with soft validation via findById
    - Apply sanitize on update and delete responses"

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -3,7 +3,7 @@ import { UsersService } from './users.service';
 import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { NotFoundException } from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
 
 const makeMockUser = (overrides = {}) => ({
   id: '1',
@@ -43,8 +43,75 @@ describe('UserService', () => {
 
       const result = await service.findById('1');
 
-      expect(result).not.toHaveProperty('PasswordHash');
+      expect(result).not.toHaveProperty('passwordHash');
       expect(result).toHaveProperty('id', '1');
+    });
+  });
+
+  describe('Create', () => {
+    test('Should return 400 and Email already exist', async () => {
+      prisma.user.findUnique.mockResolvedValue(makeMockUser());
+
+      await expect(
+        service.create({
+          name: 'name',
+          email: 'email@email.com',
+          password: '123',
+        }),
+      ).rejects.toThrow(ConflictException);
+    });
+
+    test('Should return user created whithout password', async () => {
+      prisma.user.findUnique.mockResolvedValue(null);
+      prisma.user.create.mockResolvedValue(makeMockUser());
+
+      const result = await service.create({
+        name: 'Will',
+        email: 'will@email.com',
+        password: 'hash123',
+      });
+      expect(result).not.toHaveProperty('passwordHash');
+      expect(result).toHaveProperty('id', '1');
+    });
+  });
+
+  describe('Update', () => {
+    test('should throw NotFoundException if the user does not exist', async () => {
+      prisma.user.findUnique.mockResolvedValue(null);
+
+      await expect(service.findById('not-exist')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    test('Should return user updated', async () => {
+      prisma.user.findUnique.mockResolvedValue(makeMockUser());
+      prisma.user.update.mockResolvedValue(makeMockUser({ name: 'edu' }));
+
+      const result = await service.update('1', { name: 'edu' });
+
+      expect(result).toHaveProperty('name', 'edu');
+      expect(result).not.toHaveProperty('passwordHash'); // faltou isso
+    });
+  });
+
+  describe('Delete', () => {
+    test('should throw NotFoundException if the user does not exist', async () => {
+      prisma.user.findUnique.mockResolvedValue(null);
+
+      await expect(service.findById('not-exist')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    test('Should return deleted user', async () => {
+      prisma.user.findUnique.mockResolvedValue(makeMockUser());
+      prisma.user.delete.mockResolvedValue(makeMockUser());
+
+      const result = await service.delete('1');
+
+      expect(result).toHaveProperty('id', '1');
+      expect(result).not.toHaveProperty('passwordHash'); // faltou isso
     });
   });
 });
